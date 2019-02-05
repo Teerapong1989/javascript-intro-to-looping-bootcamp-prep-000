60ae96934420c66f9b9290d76420e547c566aa22
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 01:36:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,6 +17,17 @@ function whileLoop(num) {
   } return "done"
 }
 
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
+
 function doWhileLoop(){
   
 }
