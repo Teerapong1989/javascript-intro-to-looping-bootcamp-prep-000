20811a948ddc417db82ea2c3551eb0e75e6423d8
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 01:40:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ var forLoop = function(array) {
   for (var i = 0; i < 25; i++) {
     if(i === 1){
       array.push("I am 1 strange loop")
-    } 
+    } else
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
   return array;
