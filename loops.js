e24e45be8f2257d70b29e8c322754cb735bc866c
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 01:47:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,38 +1,11 @@
-var forLoop = function(array) {
-  for (var i = 0; i < 25; i++) {
-    if(i === 1){
-      array.push("I am 1 strange loop");
-    } else {
-    array.push(`"I am ${i} strange loop"`);
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if (i === 1){
+      array.push("I am 1 strange loop.");
+    }
+      else {
+        array.push('I am ' + i + ' strange loops.')
+      }
+    }
+    return array;
   }
-  }
-  return array;
-}
-
-
-function whileLoop(num) {
-  while(num > 0){
-    console.log(--num)
-  } return "done"
-}
-
-var i = 0;
- 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
- 
-do {
-  console.log("doo-bee-doo-bee-doo");
-} while (incrementVariable() < 5);
-
-function doWhileLoop(array){
-	function maybeTrue() {
-  return Math.random() >= 0.5
-	}
-	do {
-		array.pop();
-	} while(array.length > 0 && maybeTrue());
-	return array;
-} 
